9e6afe9bc322e1026cd8b1577566417cedb035d9
isacarrd <rcpessoaluso@gmail.com>
2026-05-27 12:18:07 -0300

    create: componente de header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>GAMEFLIX</h1>
+      <nav>
+        <a href="#new">New Games</a>
+        <a href="#classic">Classic Games</a>
+        <a href="#all">All Games</a>
+      </nav>
+    </header>
+  )
+}
+export default Header;
