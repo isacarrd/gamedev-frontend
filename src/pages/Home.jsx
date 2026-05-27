1666ad431fb2c3a3312907c43333000dbd00c3bd
isacarrd <rcpessoaluso@gmail.com>
2026-05-27 12:17:34 -0300

    create: página inicial/home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import { games } from "../data/games";
+import FeaturedGame from "../components/FeaturedGame";
+import GameRow from "../components/GameRow";
+import Header from "../components/Header";
+
+function Home() {
+  const featuredGame = games.find((game) => game.featured === true);
+
+  const categories = ["Releases", "Action", "Classic Games", "Horror"];
+  return (
+    <div>
+      <Header />
+      <FeaturedGame game={featuredGame} />
+      <main className="content">
+        {categories.map((category) => {
+          const filteredGames = games.filter(
+            (game) => game.category === category
+          );
+
+          return (
+            <GameRow key={category} title={category} games={filteredGames} />
+          );
+        })}
+      </main>
+    </div>
+  );
+}
+
+export default Home;
