5de2c41d41d7827d13030c4b1d13d12ebefe4c7c
isacarrd <rcpessoaluso@gmail.com>
2026-05-27 12:18:31 -0300

    create: componente de card mostrado na tela inicial/home

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom"; 
+
+function GameCard({ game }) {
+
+  const navigate = useNavigate(); 
+
+  return (
+    <div
+      className="movie-card" 
+      onClick={() => navigate(`/game/${game.id}`)} 
+    >
+      <img src={game.coverImage} alt={game.title} />
+
+      <h3>{game.title}</h3>
+      <span>{game.type}</span>
+
+    </div>
+  );
+}
+
+export default GameCard; 
