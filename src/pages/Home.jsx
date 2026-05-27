b1034d932a8ae60f560df9b05d177a2530da7c14
isacarrd <rcpessoaluso@gmail.com>
2026-05-27 13:00:30 -0300

    update: melhoria no filtro de jogos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,14 @@ import Header from "../components/Header";
 function Home() {
   const featuredGame = games.find((game) => game.featured === true);
 
-  const categories = ["Releases", "Action", "Classic Games", "Horror"];
+  const categories = ["Releases", "Action", "Classic Games", "Horror", "Race"];
   return (
     <div>
       <Header />
       <FeaturedGame game={featuredGame} />
       <main className="content">
         {categories.map((category) => {
-          const filteredGames = games.filter(
-            (game) => game.category === category
-          );
+          const filteredGames = games.filter((game) => game.category && game.category.includes(category));
 
           return (
             <GameRow key={category} title={category} games={filteredGames} />
